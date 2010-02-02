a2ea547db823562a18602802a454dae3f32a5481
FND <fnd@oda.(none)>
2010-02-02 15:36:07 +0000

    various adjustments to xMobile macro

--- presenter.js
+++ presenter.js
@@ -3,18 +3,20 @@
 
 config.macros.xMobile = {
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
-		tiddler = store.getTiddler(params[0]);
-		var template = "!%0\n%1\n!%2\n%3";
-		text = template.format([
-			"Hazard Notes",
-			tiddler.fields["tasknotesds.@1.notetext"],
-			"Warning Notes",
-			tiddler.fields["tasknotesds.@2.notetext"]
-		]);
-		wikify(text, place)
+		var trigger = params[0];
+		if(tiddler.tags.contains(trigger)) {
+			var template = "!%0\n%1\n!%2\n%3";
+			text = template.format([
+				tiddler.title,
+				"Hazard Notes",
+				tiddler.fields["tasknotesds.@1.notetext"],
+				"Warning Notes",
+				tiddler.fields["tasknotesds.@2.notetext"]
+			]);
+			wikify(text, place)
+		}
 	}
 };
 
 })();
 //}}}
-
--- processor.js
+++ processor.js
@@ -5,7 +5,7 @@ var data = store.getTiddler("data").text;
 eval("data = " + data);
 
 store.saveTiddler(data.title, data.title, data.text, data.modifier,
-	Date.convertFromYYYYMMDDHHMM(data.modified), data.tags, data.fields, true,
+	Date.convertFromYYYYMMDDHHMM(data.modified), ["job"], data.fields, true,
 	Date.convertFromYYYYMMDDHHMM(data.created));
 
 })();
