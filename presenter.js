3579d22407a9a118a8b730e82de4f884b3f2a5ff
FND <fnd@oda.(none)>
2010-02-02 16:31:15 +0000

    using field as xMobile trigger

--- presenter.js
+++ presenter.js
@@ -3,11 +3,9 @@
 
 config.macros.xMobile = {
 	handler: function(place, macroName, params, wikifier, paramString, tiddler) {
-		var trigger = params[0];
-		if(tiddler.tags.contains(trigger)) {
+		if(tiddler.fields["tasktypeds.tasktypeid"]) {
 			var template = "!%0\n%1\n!%2\n%3";
 			text = template.format([
-				tiddler.title,
 				"Hazard Notes",
 				tiddler.fields["tasknotesds.@1.notetext"],
 				"Warning Notes",
