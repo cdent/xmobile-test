e2c7f44d7a3542910fd8189b0bede189fe84c52a
FND <fnd@oda.(none)>
2010-02-16 13:08:53 +0000

    updated data mapping

--- tiddlywebplugins/xmobile/__init__.py
+++ tiddlywebplugins/xmobile/__init__.py
@@ -1,5 +1,5 @@
-
 from tiddlyweb.util import merge_config
+
 
 NEW_CONFIG = {'log_level': 'DEBUG',
         'server_store': ['tiddlywebplugins.diststore', {
@@ -11,10 +11,11 @@ NEW_CONFIG = {'log_level': 'DEBUG',
             }],
         'system_plugins': ['tiddlywebwiki'],
         'twanager_plugins': ['tiddlywebwiki'],
-        'mappingsql.table': 'xmobile',
+        'mappingsql.table': 'jobs',
         'mappingsql.bag': 'xmobile',
-        'mappingsql.id_column': 'taskidentifier',
+        'mappingsql.id_column': 't_id',
         }
+
 
 def init(config):
     merge_config(config, NEW_CONFIG)
